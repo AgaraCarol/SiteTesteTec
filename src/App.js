f702115f458fd9dd65870044df3cf77aacf3d7a8
Ana Caroline Alves <anacarolinedacruzalves@gmail.com>
2025-11-27 17:30:58 -0300

    Projeto React Router com JSON Server - CRUD completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1- cconfig react router
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// components 
+import Navbar from './components/Navbar';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About'
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router!</h1>
+      <BrowserRouter>
+      {/* 2 - links com react router*/}
+      <Navbar />
+      <Routes>
+        <Route path="/" element ={<Home />} />
+        <Route path="/about" element ={<About />} />
+      {/* 4 - Rota dinamica*/}
+        <Route path = "/products/:id" element={<Product />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
